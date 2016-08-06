4efc35f3f3c90851b29888c2166135613790235b
zulvaf <zz_vha91@yahoo.com>
2016-08-07 01:18:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mochamadtry/myapplication/SplashPage.java
@@ -0,0 +1,24 @@
+package com.example.mochamadtry.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+    }
+
+    public void onSplashPageClick(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        //Starts the 'questions' activity
+        startActivity(intent);
+        //Closes this activity
+        finish();
+    }
+}
